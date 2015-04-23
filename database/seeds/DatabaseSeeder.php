7b2fc56a6408cb21fdf40d8e12d302fe94d4ff50
wanyuan <798401789@qq.com>
2015-04-24 00:14:27 +0800

    test some things

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
+        Model::unguard();                   //取消数据库保护，直接写入数据库
 		$this->call("UserTableSeeder");
         $this->call("ArticleTableSeeder");
         $this->command->info("data is all seeded!");
