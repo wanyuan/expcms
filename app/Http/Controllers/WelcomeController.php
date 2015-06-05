cac0e2721ea90ee23cb9bc9616007eb5bfae6b80
adminstrator <adminstrator@wanyuan.local>
2015-06-05 15:26:05 +0800

    remove the contruct return

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,45 +4,46 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Input;
 
-class WelcomeController extends Controller {
+class WelcomeController extends Controller
+{
 
-	/*
-	|--------------------------------------------------------------------------
-	| 网站首页控制器
-	|--------------------------------------------------------------------------
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | 网站首页控制器
+    |--------------------------------------------------------------------------
+    */
 
-	/**
-	 * 构造函数
-	 *
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
+    /**
+     * 构造函数
+     *
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 
-	/**
-	 * 网站首页展示
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return view('welcome');
+    /**
+     * 网站首页展示
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
 
-	}
+    }
 
     /**
      * 关于我们介绍
      */
-    public function about(){
+    public function about()
+    {
 //        echo captcha_img();
-        Input::get('myname');
+//        Input::get('myname');
 
-        echo trans('validation.before' ,['attribute' => '日期','date' => Carbon::now()]);
+        echo trans('validation.before', ['attribute' => '日期', 'date' => Carbon::now()]);
 
     }
-
 
 
 }
